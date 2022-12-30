0e264c585d39e810a34b3c6b149b8e64a9238579
Mirko <mirkoavramovicsluzbeni@gmail.com>
2022-12-30 23:33:23 +0100

    Added new view

--- match_results/urls.py
+++ match_results/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from match_results import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('match-results/', views.match_list),
+    path('match-results/<int:id>', views.match_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
